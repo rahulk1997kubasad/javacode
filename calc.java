185fc4d3cea6572fddf7e4cdf323dacea4573e3b
Rahul <rahulkubasad.rk@gmail.com>
2022-02-26 10:33:07 +0530

    hello

---
+++ calc.java
@@ -0,0 +1,10 @@
+public class calc{
+    public static void main(String args[])
+    {
+        int a,b;
+        a=20;
+        b=45;
+        int c=a+b;
+        System.out.println("Sum is "+c);
+    }
+}
